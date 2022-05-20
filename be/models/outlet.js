9ca71a61af3c07d06b98a725d345bf6b5fe45853
Davina Arafa <72923009+dappiess@users.noreply.github.com>
2022-05-20 18:14:13 +0700

    berkah ukl

---
+++ be/models/outlet.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class outlet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasOne(models.users, {
+        foreignKey: "id_outlet" , as: "users"
+      })
+      this.hasOne(models.transaksi, {
+        foreignKey: "id_outlet" , as: "transaksi"
+      })
+    }
+  }
+  outlet.init({
+    id_outlet: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nama_outlet: DataTypes.STRING,
+    alamat: DataTypes.STRING,
+    tlp: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'outlet',
+    tableName: 'outlet'
+  });
+  return outlet;
+};
